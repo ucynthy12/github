48937683da082440bbb91cfb25a3dc52213350fe
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-11-08 22:37:45 +0200

    added path to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{UsersComponent} from './users/users.component';
+import { ReposComponent } from './repos/repos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'user',component:UsersComponent},
+  {path:'repos',component:ReposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
